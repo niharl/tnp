2ec55dddfd36e33e430022d16de15c8246836b42
Nihar Lohan <lohannihar@gmail.com>
2025-12-01 21:32:34 +0000

    new script to bulk generate reversed gps

--- experiments/generate_gps.py
+++ experiments/generate_gps.py
@@ -3,7 +3,7 @@ import os
 import lightning.pytorch as pl
 import torch
 from omegaconf import OmegaConf
-from plot import plot
+from plot import plot_gps
 
 import wandb
 from tnp.utils.data_loading import adjust_num_batches
@@ -12,39 +12,52 @@ from tnp.utils.lightning_utils import LitWrapper, LogPerformanceCallback
 from datetime import datetime
 
 def main():
+    timestamp = datetime.now().strftime("%H:%M:%S %d/%m/%Y")
+
     experiment = initialize_experiment()
-    batch = experiment.generator.generate_batch()
-    
-    # prepare dict from Batch dataclass instance `batch`
-    batch_dict = {
-        "x": batch.x.cpu(),  # keep as tensors
-        "y": batch.y.cpu(),
-        "xt": batch.xt.cpu(),
-        "yt": batch.yt.cpu(),
-        "xc": batch.xc.cpu(),
-        "yc": batch.yc.cpu(),
-        "meta": {"seed": 42, "source": "generate_v1"},
-    }
 
-    # If gt_pred exists and has kernel/likelihood
-    if batch.gt_pred is not None:
-        gt_pred = batch.gt_pred
-        batch_dict["gt_pred"] = {
-            "kernel_class": gt_pred.kernel.__class__.__name__,
-            "likelihood_class": gt_pred.likelihood.__class__.__name__,
-            "kernel_state": gt_pred.kernel.state_dict(),
-            "likelihood_state": gt_pred.likelihood.state_dict(),
+    wandb.init(project="gp-bulk-generation-tests", name=f'N_c = {experiment.generator.min_nc}; {timestamp}')
+
+    batches = []
+    for _ in range(experiment.params.total_batches):
+        batch = experiment.generator.generate_batch()
+        
+        # prepare dict from Batch dataclass instance `batch`
+        batch_dict = {
+            "x": batch.x.cpu(),  # keep as tensors
+            "y": batch.y.cpu(),
+            "xt": batch.xt.cpu(),
+            "yt": batch.yt.cpu(),
+            "xc": batch.xc.cpu(),
+            "yc": batch.yc.cpu(),
+            "meta": {"seed": experiment.misc.seed, 
+                     "source": "generate_v1"},
         }
-    
-    save_dir = "./experiments/datasets"
-    os.makedirs(save_dir, exist_ok=True)
 
-    timestamp = datetime.now().strftime("%H%M%S_%d-%m-%Y")
-    filename = f"batch_{timestamp}.pt"
-    save_path = os.path.join(save_dir, filename)
+        batches.append(batch)
 
-    torch.save(batch_dict, save_path)
+        # If gt_pred exists and has kernel/likelihood
+        #if batch.gt_pred is not None:
+        if False: 
+            gt_pred = batch.gt_pred
+            batch_dict["gt_pred"] = {
+                "kernel_class": gt_pred.kernel.__class__.__name__,
+                "likelihood_class": gt_pred.likelihood.__class__.__name__,
+                "kernel_state": gt_pred.kernel.state_dict(),
+                "likelihood_state": gt_pred.likelihood.state_dict(),
+            }
+        
+            save_dir = "./experiments/datasets"
+            os.makedirs(save_dir, exist_ok=True)
 
+
+            timestamp = datetime.now().strftime("%H%M%S_%d-%m-%Y")
+            filename = f"batch_{timestamp}.pt"
+            save_path = os.path.join(save_dir, filename)
+
+            torch.save(batch_dict, save_path)
+
+    plot_gps(batches)
 
 if __name__ == "__main__":
     torch.set_float32_matmul_precision("high")
